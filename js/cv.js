af68bdfc6edb84dd4ade321537175d549ababa9e
Braian Medina <beybraian@gmail.com>
2023-03-03 23:20:18 -0300

    Implementación de Random User para rellenar los datos

--- js/cv.js
+++ js/cv.js
@@ -1,5 +1,29 @@
 $(function() {
+  $(".sobreMi").hide();
     $.scrollify({
       section : "section",
       scrollSpeed: 100,
     });});
+$("#fotoPerfilPequeña").click(function(){
+  $(".sobreMi").toggle();
+});
+
+$.ajax({
+  url: 'https://randomuser.me/api/',
+  dataType: 'json',
+  success: function(data) {
+    reemplazarDatos(data);
+  }
+});
+function reemplazarDatos(data){
+  $(".nombre").text(data.results[0].name.first);
+  $(".apellido").text(data.results[0].name.last);
+  $(".genero").text(data.results[0].gender);
+  $(".edad").text(data.results[0].dob.age);
+  $(".fotoPerfil").attr("src", data.results[0].picture.large)
+  $("#fotoPerfilPequeña").children().attr("src", data.results[0].picture.thumbnail)
+
+  $(".numeroTelefono").text(data.results[0].cell);
+  $(".correo").text(data.results[0].email);
+  $(".direccion").text(data.results[0].location.street.name + " "+ data.results[0].location.street.number);
+}
